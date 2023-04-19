e5fdefef0172127a47a6d8993627b67af2ebbe26
csengekulker <k71608883429@ktch.hu>
2023-04-19 10:32:14 +0200

    init

---
+++ app/Http/Controllers/ShipController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ship;
+use Illuminate\Http\Request;
+
+class ShipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ship $ship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ship $ship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ship $ship)
+    {
+        //
+    }
+}
